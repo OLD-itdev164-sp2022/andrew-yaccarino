66b1a7e09d0a780b1130ed4b9876ca69d895b855
AndrewYacc <78662462+AndrewYacc@users.noreply.github.com>
2022-04-17 18:35:12 -0500

    Completed Activity 7

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -11,7 +11,9 @@ import { useStaticQuery, graphql } from "gatsby"
 import styled, { ThemeProvider } from 'styled-components'
 
 import { Gray } from './themes/Gray'
-import { Header } from "./Header"
+import { Header } from './Header'
+import { Main } from './Main'
+import { Footer } from "./Footer"
 import GlobalStyle from './GlobalStyle'
 
 const Content = styled.div`
@@ -37,16 +39,12 @@ const Layout = ({ children }) => {
       <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title || `Title`} />
       <Content>
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
+        <Main>{children}</Main>
+        <Footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </Footer>
       </Content>
     </ThemeProvider>
   )
